66616e0c6dadc79ab3f2c3fe82a2cc733a35f9fc
Justin Singleton <38257985+JustinSingleton303@users.noreply.github.com>
2021-03-02 23:25:44 -0700

    jQuery event handler added

--- jquery/index.js
+++ jquery/index.js
@@ -1,6 +1,12 @@
 /* the script file for this project
    jQuery examples following*/
 
-$("h1").addClass("big-title");
+//$("h1").addClass("big-title");
 
-$("button");
+$("button").text("supBitches!!");
+
+$("a").attr("href", "https://www.yahoo.com");
+
+$("h1").click(function(){
+  alert("h1 clicked, n shit")
+});
